0023a865b2370e9fc69ff85a0e86dd72d3943eb8
Vanille-N <neven.villani@gmail.com>
2020-08-10 19:47:03 +0200

    simpler if jumpdir is a completion *command* not a completion *function*

--- jumpdir.sh
+++ jumpdir.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-_jumpdir()
-{
-    local cur prev words cword
-    _init_completion || return
-
-
-    local IFS=$'\n' i j k
-
-    compopt -o filenames
-
-    COMPREPLY=($( jumpdir "$cur" ))
-    return
-}
-if shopt -q cdable_vars; then
-    complete -v -F _jumpdir -o nospace cd pushd
-else
-    complete -F _jumpdir -o nospace cd pushd
-fi
+complete -C jumpdir cd
